5c208330bc444d862eab39cee9241a6841b497c6
Ayush kumar <krayush294@gmail.com>
2024-12-16 14:17:03 +0530

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,31 @@
+import Android from "./Android";
+import Ios from "./Ios";
+import Web from "./Web";
+import Windows from "./Windows";
+
+ export const links = [
+ {
+  id: "0",
+  title: "Ios",
+  icon: <Ios />,
+  url: "#",
+ },
+ {
+  id: "1",
+  title: "Android",
+  icon: <Android />,
+  url: "#",
+ },
+ {
+  id: "2",
+  title: "Windows",
+  icon: <Windows />,
+  url: "#",
+ },
+ {
+  id: "3",
+  title: "Web",
+  icon: <Web />,
+  url: "#",
+ },
+];
